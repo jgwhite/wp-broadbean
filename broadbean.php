a3eba6165d27fc77a2522870285aca76b0c87258
Jamie White <jamie@jgwhite.co.uk>
2012-04-18 23:09:26 +0100

    inbox works

--- broadbean.php
+++ broadbean.php
@@ -33,6 +33,7 @@ class Broadbean {
 
   function init() {
     self::create_post_type();
+    self::add_rewrite_rule();
   }
 
   function create_post_type() {
@@ -61,13 +62,25 @@ class Broadbean {
     );
   }
 
+  function add_rewrite_rule() {
+    $endpoint = get_option('broadbean_endpoint');
+    if (empty($endpoint)) $endpoint = 'broadbean-inbox';
+    $endpoint = preg_replace('/^\/+/', '', $endpoint);
+
+    $url = plugins_url('inbox.php',  'broadbean/broadbean.php');
+    $url = str_replace(site_url(), '', $url);
+    $url = preg_replace('/^\/+/', '', $url);
+
+    add_rewrite_rule("$endpoint$", $url, 'top');
+  }
+
   function create_admin_menu() {
     add_submenu_page(
       'options-general.php',
       'Broadbean',
       'Broadbean',
       'manage_options',
-      'wp-broadbean',
+      'broadbean',
       array('Broadbean', 'admin_menu')
     );
   }
@@ -89,7 +102,8 @@ class Broadbean {
         <tr valign="top">
           <th scope="row"><label for="broadbean_endpoint">Endpoint Path</label></th>
           <td><input type="text" name="broadbean_endpoint" id="broadbean_endpoint"
-                     value="<?php echo get_option('broadbean_endpoint') ?>"></td>
+                     value="<?php echo get_option('broadbean_endpoint') ?>"
+                     placeholder="broadbean-inbox"></td>
         </tr>
         <tr valign="top">
           <th scope="row"><label for="broadbean_username">Username</label></th>
@@ -114,5 +128,11 @@ class Broadbean {
 add_action('init', array('Broadbean', 'init'));
 add_action('admin_init', array('Broadbean', 'register_settings'));
 add_action('admin_menu', array('Broadbean', 'create_admin_menu'));
+
+add_filter('plugin_action_links_broadbean/broadbean.php', 'broadbean_settings_link');
+function broadbean_settings_link($links) {
+  array_unshift($links, '<a href="options-general.php?page=broadbean">Settings</a>');
+  return $links;
+}
 
 ?>
--- inbox.php
+++ inbox.php
@@ -3,7 +3,60 @@
 Broadbean Inbox Handler
 */
 
-require('wp-load.php');
+$root = $_SERVER['DOCUMENT_ROOT'];
+
+if (file_exists($root.'/wp-load.php')) {
+  require_once($root.'/wp-load.php');
+} else {
+  echo 'Uh oh, cannot find wp-load.php';
+}
+
+$username = get_option('broadbean_username');
+$password = get_option('broadbean_password');
+
+$xml = file_get_contents('php://input');
+$params = json_decode(json_encode(simplexml_load_string($xml)), 1);
+
+if ($params['username'] !== $username || $params['password'] !== $password) {
+  echo 'Incorrect username and/or password';
+  die();
+}
+
+$job_post_id = wp_insert_post(array(
+  'post_type'     => 'job',
+  'post_title'    => $params['job_title'],
+  'post_category' => array($params['job_category']),
+  'post_content'  => $params['job_description']
+));
+
+if ($job_post_id != 0) {
+  add_post_meta($job_post_id , 'Job Type'          , $params['job_type']          , true);
+  add_post_meta($job_post_id , 'Contact Name'      , $params['contact_name']      , true);
+  add_post_meta($job_post_id , 'Contact Email'     , $params['contact_email']     , true);
+  add_post_meta($job_post_id , 'Contact Telephone' , $params['contact_telephone'] , true);
+  add_post_meta($job_post_id , 'Contact URL'       , $params['contact_url']       , true);
+  add_post_meta($job_post_id , 'Days to Advertise' , $params['days_to_advertise'] , true);
+  add_post_meta($job_post_id , 'Application Email' , $params['application_email'] , true);
+  add_post_meta($job_post_id , 'Application URL'   , $params['application_url']   , true);
+  add_post_meta($job_post_id , 'Job Reference'     , $params['job_reference']     , true);
+  add_post_meta($job_post_id , 'Job Title'         , $params['job_title']         , true);
+  add_post_meta($job_post_id , 'Job Type'          , $params['job_type']          , true);
+  add_post_meta($job_post_id , 'Job Duration'      , $params['job_duration']      , true);
+  add_post_meta($job_post_id , 'Job Startdate'     , $params['job_startdate']     , true);
+  add_post_meta($job_post_id , 'Job Skills'        , $params['job_skills']        , true);
+  add_post_meta($job_post_id , 'Job Description'   , $params['job_description']   , true);
+  add_post_meta($job_post_id , 'Job Location'      , $params['job_location']      , true);
+  add_post_meta($job_post_id , 'Job Industry'      , $params['job_industry']      , true);
+  add_post_meta($job_post_id , 'Salary Currency'   , $params['salary_currency']   , true);
+  add_post_meta($job_post_id , 'Salary From'       , $params['salary_from']       , true);
+  add_post_meta($job_post_id , 'Salary To'         , $params['salary_to']         , true);
+  add_post_meta($job_post_id , 'Salary Per'        , $params['salary_per']        , true);
+  add_post_meta($job_post_id , 'Salary Benefits'   , $params['salary_benefits']   , true);
+  add_post_meta($job_post_id , 'Salary'            , $params['salary']            , true);
+}
+
+echo 'Created, thanks!';
+
+die();
 
 ?>
-
